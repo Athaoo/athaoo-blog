15846660d01c980a0c99bc73f1a4e096bed3d64a
lijinchao <13522655053@163.com>
2023-08-02 11:29:19 +0800

    refactor: 摘出后端部分

--- be/src/routes/auth.js
+++
@@ -1,9 +0,0 @@
-import Router from '@koa/router'
-import { loginAdmin, registerAdmin } from '../controllers/admin.js'
-
-const router = new Router({ prefix: '/api' })
-
-router.post('/login', loginAdmin)
-router.post('/register', registerAdmin)
-
-export default router
