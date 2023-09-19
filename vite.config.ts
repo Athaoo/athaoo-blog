e86760efdedd7310247c54e7cd2dc7583097a41f
lijinchao <13522655053@163.com>
2023-09-19 15:06:03 +0800

    chore: 增加打包体积分析工具

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import { visualizer } from 'rollup-plugin-visualizer'
 import path from 'path'
 
 const { resolve } = path
@@ -38,7 +39,7 @@ export default defineConfig(({ command, mode }) => {
       outDir: resolve(__dirname, './dist'),
       emptyOutDir: true, //清空dist
     },
-    plugins: [react()],
+    plugins: [react(), visualizer()],
     publicDir: resolve(__dirname, './public'),
     base: env.VITE_ROUTER_BASE ?? '',
   }
